7080d411cc8a623f2cec1df352aeaab2010bd570
Petra <soininenpetra@gmail.com>
2025-04-19 19:37:32 +0300

    burgermenu for mobile

--- exersice work/main.js
+++ exersice work/main.js
@@ -29,3 +29,12 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerButton = document.querySelector(".hamburger-button");
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  hamburgerButton.addEventListener("click", () =>
+    mobileMenu.classList.toggle("active")
+  );
+});
